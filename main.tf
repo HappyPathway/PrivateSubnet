c59f71ed318df39a36d4d59d1af9a8cc19475767
Dave Arnold <dave@alphonso.tv>
2018-03-11 09:53:04 -0700

    working on private subnet

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnet" {
-  vpc_id                  = "${aws_vpc.vpc.id}"
+  vpc_id                  = "${var.vpc_id}"
   cidr_block              = "${var.subnet_cidr}"
   map_public_ip_on_launch = "${var.public_ip}"
 
--- vars.tf
+++ vars.tf
@@ -7,6 +7,4 @@ variable "subnet_cidr" {
   default = "10.0.2.0/24"
 }
 
-variable "company_name" {}
-variable "org_name" {}
 variable "public_subnet_id" {}
