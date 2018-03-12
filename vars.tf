c759ffec7b1f0302f69e51f4399af8ab3c7fc4e6
Dave Arnold <dave@alphonso.tv>
2018-03-12 14:49:12 -0700

    updating vars

--- vars.tf
+++ vars.tf
@@ -1,10 +1,11 @@
 variable "subnet_name" {}
 variable "vpc_id" {}
 variable "public_ip" {
-  default = true
+  default = false
 }
 variable "subnet_cidr" {
   default = "10.0.2.0/24"
 }
 
 variable "public_subnet_id" {}
+variable "availability_zone" {}
